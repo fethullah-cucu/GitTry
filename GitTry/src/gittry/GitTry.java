1b5341683f9dc0e2da7c18a2ef5ecca2d9f5da0f
2022221 <2022221@student.cct.ie>
2023-11-29 13:36:49 +0000

    Update GitTry.java

--- GitTry/src/gittry/GitTry.java
+++ GitTry/src/gittry/GitTry.java
@@ -17,6 +17,7 @@ public class GitTry {
         System.out.println("Hello World");    
         System.out.println("Hello World 2");    
         System.out.println("hellooo world 4");
+        System.out.println("hello jane");
     }
     
 }
